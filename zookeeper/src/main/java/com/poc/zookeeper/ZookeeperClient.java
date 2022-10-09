ba237160620994c1f077307a460e97e835bbe956
deepak-ctax <kumar.deepak@cleartax.in>
2022-10-09 11:21:43 +0530

    Oct 9th 2022

--- zookeeper/src/main/java/com/poc/zookeeper/ZookeeperClient.java
+++ zookeeper/src/main/java/com/poc/zookeeper/ZookeeperClient.java
@@ -4,16 +4,34 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
 
 public class ZookeeperClient {
-    private final CuratorFramework curatorFramework;
+    private static final Logger log = LoggerFactory.getLogger(ZookeeperClient.class.getName());
+
+    private static final ZookeeperClient INSTANCE = new ZookeeperClient();
+
+    private final CuratorFramework client;
+
+    public static ZookeeperClient instance() {
+        return INSTANCE;
+    }
 
     private ZookeeperClient() {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
-        this.curatorFramework = CuratorFrameworkFactory.newClient("localhost:9092", retryPolicy);
+        this.client = CuratorFrameworkFactory.newClient("localhost:2181", retryPolicy);
+        client.start();
     }
 
-    public String createNode() {
-        return "Node created";
+    public String createNode(String path) {
+        try {
+            this.client.create().forPath(path, "Hello".getBytes(StandardCharsets.UTF_8));
+        } catch (Exception e) {
+            log.error("Error creating node with path: {}", path);
+        }
+        return String.format("Path %s created", path);
     }
 }
