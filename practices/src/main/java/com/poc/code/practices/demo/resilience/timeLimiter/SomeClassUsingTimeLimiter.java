401527242deafa33e89debdcee466ada78fd2ef4
deepak <deep.rnj@gmail.com>
2023-10-18 20:23:15 +0530

    Time limiting demo for completable futures

--- practices/src/main/java/com/poc/code/practices/demo/resilience/timeLimiter/SomeClassUsingTimeLimiter.java
+++ practices/src/main/java/com/poc/code/practices/demo/resilience/timeLimiter/SomeClassUsingTimeLimiter.java
@@ -1,5 +1,6 @@
 package com.poc.code.practices.demo.resilience.timeLimiter;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,10 +17,17 @@ public class SomeClassUsingTimeLimiter implements SomeInterfaceUsingTimeLimiter 
     private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(2);
 
     @Override
+    @Bulkhead(name = "downstream", type = Bulkhead.Type.THREADPOOL)
     @TimeLimiter(name = "downstream")
     public CompletableFuture<String> doSomethingAsync() throws InterruptedException {
-        Thread.sleep(2000);
-        return CompletableFuture.supplyAsync(() -> "Test result");
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Test result";
+        });
     }
 
     public CompletableFuture<String> doSomethingAsyncWithTimeLimit() {
